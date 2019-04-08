8804949881eb872f0e86b70f89da50bda19ef509
Franco Brunetta Pan <francovski2@gmail.com>
2019-04-08 13:00:05 -0300

    Added Moment, Fixed Date Validation, Added Config File, Fixed Build for Windows

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+const fs = require('fs');
+const tz = require('./assets/tz.json');
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const puppeteer = require('puppeteer');
 const dateFormat = require('dateformat');
-var player = require('play-sound')({player: 'C:\\Program Files (x86)\\MPlayer for Windows\\MPlayer.exe'});
+const player = require('play-sound')({player: config.mPlayerPath});
+const moment = require('moment-timezone');
 
+// Global Variables and set-ups
+moment.tz.setDefault(config.timezoneCode);
+moment.tz.add(tz.zones);
+var exports = module.exports = {};
+const isPkg = typeof process.pkg !== 'undefined';
 const urls = {
     availability: 'https://www.passaportonline.poliziadistato.it/GestioneDisponibilitaAction.do?codop=getDisponibilitaCittadino',
     login: 'https://www.passaportonline.poliziadistato.it/logInCittadino.do'
 };
-var exports = module.exports = {};
 
 exports.timeout = function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -15,8 +23,8 @@ exports.timeout = function timeout(ms) {
 exports.login = async function login(page) {
     await page.goto(urls.login);
     await page.waitForSelector('#codiceFiscale')
-    await page.type('#codiceFiscale', 'BRNFNC95H02Z602B')
-    await page.type('#password', 'Fbruneta2019!')
+    await page.type('#codiceFiscale', config.username)
+    await page.type('#password', config.password)
     await page.click('#btnSub')
     await Promise.all([
         page.goto(urls.availability),
@@ -24,13 +32,15 @@ exports.login = async function login(page) {
     ]);
 }
 
-exports.main = async function main(pageLoaded) {
-    // Set-up environment  
-    const browser = await puppeteer.launch();
-    let page = pageLoaded !== null? pageLoaded : await browser.newPage(); 
+exports.main = async function main(pageLoaded) { 
+    const limit  = moment(config.limitDate);
     let found = false;
     let triesCounter = 0;
-    
+
+    //Start Browser (Chrome/Chromium)
+    const browser = await puppeteer.launch({executablePath: isPkg ? config.chromePath: puppeteer.executablePath()});
+    let page = pageLoaded !== null? pageLoaded : await browser.newPage(); 
+
     // Find Dates
     await page.goto(urls.availability);
     while(found === false) {
@@ -56,39 +66,34 @@ exports.main = async function main(pageLoaded) {
             return arrayReturn;
         })
 
-        date = new Date(dataScraped[0]);
-        if (date < new Date().getDate() + 30) {
+        let date = moment(dataScraped[0].substring(0,10));
+        if (date <= limit) {
             for (i = 0; i < 10; i++) {
                 player.play('assets/sounds/bell.mp3', function(err){
                     if (err) throw err
-                })  
+                });
+				console.log('FOOOOOOOOOOOOOOOOOOOOOOUNDDDDDDD: ' + date.format('YYYY-MM-DD') + ' (' + dataScraped[1] + ')');
                 await this.timeout(2500);  
             }
-            console.log('AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE');
-            console.log('ACHOOOOOOOOOOOOOOOU: ' + dateFormat(date, 'yyyy-mm-dd') + ' (' + dataScraped[1] + ')');
-            //found = true;
         } else {
-            console.log('Found: ' + dateFormat(date, 'yyyy-mm-dd') + ' (' + dataScraped[1] + ')' + ' @ ' + dateFormat(new Date(), 'yyyy-mm-dd h:MM:ss TT'));
-            console.log('Re-trying in 60s...');
+            console.log('Sorry, no date availabe. Found ' + date.format('YYYY-MM-DD') + 
+                ' (' + dataScraped[1] + ')' + ' @ ' + moment().format() + 
+                ', Limit: ' + limit.format('YYYY-MM-DD')
+            );
 
-            if (triesCounter++ >= 15) { //Every 10 minutes
+            if (triesCounter++ >= 15) { //Every 15 minutes
                 let audio = player.play('assets/sounds/no.mp3', function(err){
                     if (err) throw err
                 })
-                await this.timeout(2500);
+                await this.timeout(2000);
                 audio.kill();
                 triesCounter = 0;
             }
         }
-        await this.timeout(60000);
-    }    
-    
-    
-    //await page.screenshot({path: 'example.png'});
-    
-    
+        console.log('Re-trying in ' + config.timeout + 's...');
+        await this.timeout(config.timeout * 1000);
+    }        
     await browser.close();
 }
-
 
 exports.main(null);
